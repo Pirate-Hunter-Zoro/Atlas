706e4b3b280efa242c740ba8a239c23cefc92d54
Mikey Ferguson <mtf6056@utulsa.edu>
2026-07-03 16:53:26 -0500

    set up min path sum and solved cherry pickup

--- practice/Algo-Solutions/solution.go
+++ practice/Algo-Solutions/solution.go
@@ -5744,5 +5744,89 @@ Link:
 https://leetcode.com/problems/cherry-pickup/description/
 */
 func cherryPickup(grid [][]int) int {
+	// From the editorial, consider two paths from top left to bottom right (path back is just reversed)
+	// Given r1,c1, and (WLOG) r2, we can compute c2 because the two paths must have the same number of steps in a subproblem
+	r := len(grid)
+	c := len(grid[0])
+
+	// DP setup
+	sols := make([][][]int, r)
+	for i:=range r {
+		sols[i] = make([][]int, c)
+		for j:=range c {
+			sols[i][j] = make([]int, r)
+			for k:=range r {
+				sols[i][j][k] = -2
+			}
+		}
+	}
+
+	var solve func(r1 int, c1 int, r2 int) int;
+	solve = func(r1 int, c1 int, r2 int) int {
+		// Check if valid first - constant time test
+		c2 := r1 + c1 - r2
+		if r1 >= r || r2 >= r || c1 >= c || c2 >= c {
+			return -1
+		}
+
+		if sols[r1][c1][r2] == -2 {
+			// Need to solve this problem
+			if grid[r1][c1] == -1 || grid[r2][c2] == -1 {
+				// Must stop here
+				sols[r1][c1][r2] = -1
+				// Constant 
+			} else {
+				// Need not stop
+				if r1 == r-1 && r2 == r-1 && c1 == c-1 && c2 == c-1 {
+					// Base case - at the end - only count its value once
+					sols[r1][c1][r2] = grid[r-1][c-1]
+				} else {
+					// Both people can try moving right or down - that's 2x2=4 subproblems
+					record := -1
+					
+					initial_amount := grid[r1][c1]
+					if r1 != r2 || c1 != c2 {
+						initial_amount += grid[r2][c2]
+					}
+					// Try both right
+					if solve(r1, c1+1, r2) != -1 {
+						record = max(record, initial_amount + solve(r1, c1+1, r2))
+					}
+					// Try both down
+					if solve(r1+1, c1, r2+1) != -1 {
+						record = max(record, initial_amount + solve(r1+1, c1, r2+1))
+					}
+					// Try first right, second down
+					if solve(r1, c1+1, r2+1) != -1 {
+						record = max(record, initial_amount + solve(r1, c1+1, r2+1))
+					}
+					// Try first down, second right
+					if solve(r1+1, c1, r2) != -1 {
+						record = max(record, initial_amount + solve(r1+1, c1, r2))
+					}
+
+					// Record the record
+					sols[r1][c1][r2] = record
+				}
+			}
+		}
+
+		return sols[r1][c1][r2]
+	}
+
+	return max(0, solve(0,0,0))
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+*/
+func minPathSum(grid [][]int) int {
     return 0
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
