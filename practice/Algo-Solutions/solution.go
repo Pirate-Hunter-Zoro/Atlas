c12e7919d5b6cf44fca149ad22963248bccc3ea3
Mikey Ferguson <mtf6056@utulsa.edu>
2026-07-09 17:28:29 -0500

    started reachable nodes

--- practice/Algo-Solutions/solution.go
+++ practice/Algo-Solutions/solution.go
@@ -5859,9 +5859,9 @@ func minPathSum(grid [][]int) int {
 You are given an undirected graph (the "original graph") with n nodes labeled from 0 to n - 1. 
 You decide to subdivide each edge in the graph into a chain of nodes, with the number of new nodes varying between each edge.
 
-The graph is given as a 2D array of edges where edges[i] = [u_i, v_i, cnt_i] indicates that there is an edge between nodes ui and vi in the original graph, and cnti is the total number of new nodes that you will subdivide the edge into. Note that cnt_i == 0 means you will not subdivide the edge.
+The graph is given as a 2D array of edges where edges[i] = [u_i, v_i, cnt_i] indicates that there is an edge between nodes ui and vi in the original graph, and cnt_i is the total number of new nodes that you will subdivide the edge into. Note that cnt_i == 0 means you will not subdivide the edge.
 
-To subdivide the edge [u_i, v_i], replace it with (cnt_i + 1) new edges and cnt_i new nodes. The new nodes are x1, x2, ..., xcnt_i, and the new edges are [ui, x1], [x1, x2], [x2, x3], ..., [xcnt_i-1, xcnt_i], [xcnt_i, v_i].
+To subdivide the edge [u_i, v_i], replace it with (cnt_i + 1) new edges and cnt_i new nodes. The new nodes are x1, x2, ..., xcnt_i, and the new edges are [u_i, x1], [x1, x2], [x2, x3], ..., [xcnt_{i-1}, xcnt_i], [xcnt_i, v_i].
 
 In this new graph, you want to know how many nodes are reachable from the node 0, where a node is reachable if the distance is maxMoves or less.
 
@@ -5871,7 +5871,25 @@ Link:
 https://leetcode.com/problems/reachable-nodes-in-subdivided-graph/description/?envType=problem-list-v2&envId=shortest-path
 */
 func reachableNodes(edges [][]int, maxMoves int, n int) int {
-    return 0
+	// Construct graph and keep track of the splits of each edge
+	graph := make([][][]int, len(edges))
+	for i:=range len(edges) {
+		u := edges[i][0]
+		v := edges[i][1]
+		cuts := edges[i][2]
+		graph[u] = append(graph[u], []int{v, cuts+1}) // True edge cost
+		graph[v] = append(graph[v], []int{u, cuts+1})
+	}
+	
+	// Before we consider divisions, we need to first find out how many hops it takes to reach each node from node zero
+	node_heap := datastructures.NewHeap(func(node_a []int, node_b []int) bool {
+		return node_a[1] < node_b[1]
+	})
+	reachable := 0
+	// Find the shortest path from node zero to all other nodes, and any that are within the distance (careful counting with the newly split edges) not already reached contribute to the count
+	
+	
+	return reachable
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
