cf6bb42a3ae569264d0f466b524b5c0370cd2f01
Mikey Ferguson <mtf6056@utulsa.edu>
2026-08-05 11:18:39 -0500

    set up generatetrees

--- practice/Algo-Solutions/leetcode/generatetrees/generatetrees.go
+++ practice/Algo-Solutions/leetcode/generatetrees/generatetrees.go
@@ -2,6 +2,13 @@ package generatetrees
 
 import "algo-solutions/datastructures"
 
+/*
+Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. 
+Return the answer in any order.
+
+Link:
+https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+*/
 func generateTrees(n int) []*datastructures.TreeNode {
     return []*datastructures.TreeNode{}
 }
